392fc584702dd54c290c5091440b4d801ace15ca
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-04 00:28:39 -0600

    Se desarrollo la interfaz del formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import BudgetForm from "./components/BudgetForm"
+
 function App() {
 
   return (
@@ -9,7 +11,7 @@ function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        
+        <BudgetForm/>
       </div>
     </>
   )
