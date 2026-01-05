b618641342b0c1e15f7a83a44c369698e15f5ba5
RigoDelgadillo <142523123+RigoDelgadillo@users.noreply.github.com>
2026-01-04 22:15:22 -0600

    Rigo (#4)
    * Se creo la carpeta del context de la aplicacion
    
    * Se creo el context del presupuesto
    
    * Se agrego el provider a main
    
    * Se utilizo el hook useBudget en un submit
    
    * Se elimino un caracter por error de dedo
    
    * Se creo un custom hook para los presupuestos
    
    * Se creo la funcion de validar el presupuesto para eliminar el formulario
    
    * Se creo el componente al que seran dirigidos si tienen un presupuesto valido

---
+++ src/hooks/useBudget.ts
@@ -0,0 +1,12 @@
+import { useContext } from "react"
+import { BudgetContext } from "../context/BudgetContext"
+
+export const useBudget = () => {
+  const context = useContext(BudgetContext)
+
+  if(!context){
+    throw new Error ('useBudget debe usarse en BudgetProvider')
+  }
+
+  return context
+}
