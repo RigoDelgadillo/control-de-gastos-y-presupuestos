0fbb7531415357f029f4562fe1e3793c784fa9b5
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-06 00:27:17 -0600

    Se creo el formulario para agregar el gasto

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,6 +1,79 @@
+import { categories } from "../data/categories";
 
 export default function ExpenseForm() {
   return (
-    <div>ExpenseForm</div>
+    <form className="space-y-5">
+      <legend
+        className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
+      >
+        Nuevo Gasto
+      </legend>
+
+      <div className="flex flex-col gap-2" >
+        <label
+          htmlFor="expenseName"
+          className="text-xl"
+        >
+          Nombre Gasto: 
+        </label>
+        <input 
+          type="text"  
+          placeholder="Agrega el nombre del gasto"
+          id="expenseName"
+          className="bg-slate-100 p-2"
+          name="expenseName"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2" >
+        <label
+          htmlFor="amount"
+          className="text-xl"
+        >
+          Cantidad: 
+        </label>
+        <input 
+          type="number"  
+          placeholder="Agrega la cantidad del casto: ej. 300"
+          id="amount"
+          className="bg-slate-100 p-2"
+          name="amount"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2" >
+        <label
+          htmlFor="amount"
+          className="text-xl"
+        >
+          Categoria: 
+        </label>
+
+        <select 
+          id="category"
+          className="bg-slate-100 p-2"
+          name="category"
+        >
+          <option value=""> -- Seleccione -- </option>
+          {categories.map( category => (
+            
+            <option
+              key={category.id} 
+              value={category.id}
+            >
+              {category.name}
+            </option>
+
+          ))}
+        </select>
+      </div>
+
+      <input 
+        type="submit"
+        className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+        value="Registrar Gasto"
+      />
+
+    </form>
   )
 }
