0bb3cfb0d7f3d107a91d66370ccaeda1ab2f3efa
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-05 23:12:57 -0600

    Agregue un componente para la visualizacion del presupuesto

---
+++ src/components/AmountDisplay.tsx
@@ -0,0 +1,11 @@
+
+type AmountDisplayProps = {
+  label : string,
+  amount : number,
+}
+
+export default function AmountDisplay({label, amount} : AmountDisplayProps) {
+  return (
+    <div>AmountDisplay</div>
+  )
+}
