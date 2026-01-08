aaf7d50e8e06d1b628d801aca9cd3d6baf8fec9f
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-07 23:25:19 -0600

    Se agregaron el type del calendario

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,4 +10,10 @@ export type DraftExpense = Omit<Expense, "id">
 
 type ValuePiece = Date | null;
 
-type Value = ValuePiece | [ValuePiece, ValuePiece];
+export type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+export type Category = { 
+  id: string
+  name: string
+  icon: string
+}
