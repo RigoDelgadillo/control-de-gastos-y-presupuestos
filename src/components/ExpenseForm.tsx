268499354d92d8dae4dc872f54f25ef2a437353a
Rigoberto Delgadillo <142523123+RigoDelgadillo@users.noreply.github.com>
2026-01-08 00:25:50 -0600

    Rigo (#10)
    * Se creo la carpeta del context de la aplicacion
    
    * Se creo el context del presupuesto
    
    * Se agrego el provider a main
    
    * Se utilizo el hook useBudget en un submit
    
    * Se elimino un caracter por error de dedo
    
    * Se creo un custom hook para los presupuestos
    
    * Se creo la funcion de validar el presupuesto para eliminar el formulario
    
    * Se creo el componente al que seran dirigidos si tienen un presupuesto valido
    
    * Agregue un componente para la visualizacion del presupuesto
    
    * Se creo un formateador de moneda
    
    * Se creo la sintaxis del Componente para mostrar las opciones (Aun es estatico)
    
    * Se reutilizo el componente para ver una demostracion
    
    * Se creo el modal que se utilizara mas adelante
    
    * Se agrego el modal solo cuando pase la validacion
    
    * Se instalo Heroicons y headlessui a la aplicacion
    
    * Se agrego una nueva accion al reducer
    
    * Se agrego la opcion de abrir el modal con el boton
    
    * Se cambiaron algunos componente obsoletos
    
    * Se agrego la opcion para cerrar el modal
    
    * Se agrego la accion de cerrar el modal
    
    * Se creo el componente que estara dentro del modal
    
    * Se corrigio un bug en el opacity del modal
    
    * Se crearon los datos para las categorias
    
    * Se creo el formulario para agregar el gasto
    
    * Se instalo una dependencia para facilitar la creacion de calendario
    
    * ...
    
    * Se tiparon las categorias
    
    * Se crearon los types que se utilizaran en la aplicacion
    
    * Se agrego el state del formulario y los valores del los inputs
    
    * Se elimino un console.log
    
    * Se agregaron el type del calendario
    
    * Se agregaron los eventos para que la aplicacion detecte cuando cambie un estado
    
    * Se creo un componente que muestra un error
    
    * Se creo la validacion de los campos y muestra mensaje
    
    * Se agrego la accion de agregar gastoal state
    
    * El gasto se agrega al reducer correctamente
    
    * Se le agrega un id unico a cada gasto nuevo

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,12 @@
 import { categories } from "../data/categories";
-import type { DraftExpense } from "../types";
+
+import type { DraftExpense, Value } from "../types";
 import DatePicker from 'react-date-picker';
 import 'react-calendar/dist/Calendar.css'
 import 'react-date-picker/dist/DatePicker.css'
-import { useState } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
+import ErrorMessage from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
 
 export default function ExpenseForm() {
 
@@ -14,14 +17,48 @@ export default function ExpenseForm() {
     date: new Date()
   })
 
+
+  const [error, setError] = useState('')
+  const { dispatch } = useBudget()
+
+  const handleChange = (e : ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target
+    const isAmountField = ['amount'].includes(name)
+    setExpense({
+      ...expense,
+      [name] : isAmountField ? Number(value) : value
+    })
+  }
+
+  const handleChangeDate = (value : Value)  => {
+    setExpense({
+      ...expense,
+      date: value
+    })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if(Object.values(expense).includes('')) {
+      setError('Todos los campos son obligatorios')
+      return
+    }
+
+    dispatch({type: 'add-expense', payload: { expense }})
+    
+  }
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={ handleSubmit }>
       <legend
         className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
       >
         Nuevo Gasto
       </legend>
- 
+      
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+
       <div className="flex flex-col gap-2" >
         <label
           htmlFor="expenseName"
@@ -36,6 +73,7 @@ export default function ExpenseForm() {
           className="bg-slate-100 p-2"
           name="expenseName"
           value={expense.expenseName}
+          onChange={handleChange}
         />
       </div>
 
@@ -53,6 +91,7 @@ export default function ExpenseForm() {
           className="bg-slate-100 p-2"
           name="amount"
           value={expense.amount}
+          onChange={handleChange}
         />
       </div>
 
@@ -69,6 +108,7 @@ export default function ExpenseForm() {
           className="bg-slate-100 p-2"
           name="category"
           value={expense.category}
+          onChange={handleChange}
         >
           <option value=""> -- Seleccione -- </option>
           {categories.map( category => (
@@ -95,6 +135,7 @@ export default function ExpenseForm() {
         <DatePicker
           className="bg-slate-100 p-2 border-0"
           value={expense.date}
+          onChange={handleChangeDate}
         />
       </div>
 
