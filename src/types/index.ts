4eb712ead6c0a109cb677c2e75ed3f84207e9085
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-07 22:50:02 -0600

    Se crearon los types que se utilizaran en la aplicacion

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type Expense = {
+  id : string
+  expenseName : string
+  amount : number 
+  category : string
+  date : Value
+}
+
+export type DraftExpense = Omit<Expense, "id">
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
