68fb9f846c2794be414a6f4558a0a3a076047e88
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-07 23:52:13 -0600

    Se agrego la accion de agregar gastoal state

--- src/reducers/budget-reducer.tsx
+++ src/reducers/budget-reducer.tsx
@@ -1,16 +1,21 @@
+import type { DraftExpense, Expense } from "../types"
+
 export type BudgetActions = 
   { type: 'add-budget',  payload: { budget : number } } |
   { type: 'show-modal' } |
-  { type: 'close-modal' }
+  { type: 'close-modal' } |
+  { type: 'add-expense', payload: {expense: DraftExpense }}
 
 export type BudgetState = {
   budget: number 
   modal: boolean
+  expenses: Expense[]
 }
 
 export const initialState : BudgetState = {
   budget : 0,
-  modal: false
+  modal: false,
+  expenses: []
 }
 
 export const budgetReducer = (
@@ -37,6 +42,13 @@ export const budgetReducer = (
         modal: false
       }
     }
+
+  if(action.type === 'add-expense'){
+    return {
+      ...state,
+      expenses: [...state.expenses, action.payload.expense]
+    }
+  }
 
 
   return state
