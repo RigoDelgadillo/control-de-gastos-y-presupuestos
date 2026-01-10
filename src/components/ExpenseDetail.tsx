c5c82d6c13024a03b7d8364140e714fd80d70294
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-09 21:29:30 -0600

    La lista muestra la categoria de cada gasto

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,12 +1,17 @@
+import { useMemo } from "react"
 import { formatDate } from "../helpers"
 import type { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
+import { categories } from "../data/categories"
 
 type ExpenseDetailProps = {
   expense : Expense
 }
 
 export default function ExpenseDetail({expense} : ExpenseDetailProps) {
+
+  const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
+  
   return (
     <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
 
@@ -15,6 +20,7 @@ export default function ExpenseDetail({expense} : ExpenseDetailProps) {
       </div>
 
       <div>
+        <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
         <p>{expense.expenseName}</p>
         <p className="text-slate-600 text-sm">
           {formatDate( expense.date!.toString() )}
