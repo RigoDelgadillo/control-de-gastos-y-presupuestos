6b5b228a436da3dc3941fe2a588a3578e5ee9d38
Rigo Delgadillo <rigodelgadillo10@gmail.com>
2026-01-14 22:27:55 -0600

    se agrego filtro para las categorias de los gastos

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
 import ExpenseModal from "./components/ExpenseModal"
 import ExpenseList from "./components/ExpenseList"
+import FilterByCategory from "./components/FilterByCategory"
 
 function App() {
   
@@ -32,7 +33,8 @@ function App() {
       </div>
 
       {isValidBudget && (
-        <main className="max-w-3xl mx-auto py-10">
+        <main className="max-w-3xl mx-auto py-10"> 
+          <FilterByCategory/>
           <ExpenseList/>
           <ExpenseModal/>
         </main>
